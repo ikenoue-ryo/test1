23428bc64252327cc20146c087eb2ca2ef168af6
Ryo <ryo@RyonoMacBook-Pro.local>
2020-07-26 17:27:38 +0900

    commit

--- test_app/views.py
+++ test_app/views.py
@@ -1,14 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.views import LoginView, LogoutView
-from django.shortcuts import render
-from .models import Question
-from .forms import LoginForm, SignUpForm
-from django.views.generic import TemplateView, ListView, CreateView, DetailView, UpdateView, DeleteView
 
-from .controller import main
-
-DEFAULT_ROBOT_NAME = 'Roboko'
 
 def indexfunc(request):
     user = Question.objects.get(id=request.user.id)

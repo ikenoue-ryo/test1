412753b143b4cbaf2605a4b857b76ce2ab12ecc2
Ryo <ryo@RyonoMacBook-Pro.local>
2020-07-26 20:20:44 +0900

    commmit

--- test_app/views.py
+++ test_app/views.py
@@ -2,10 +2,5 @@ from django.shortcuts import render, redirect
 
 
 def indexfunc(request):
-    user = Question.objects.get(id=request.user.id)
 
-    if request.method == 'POST':
-        user.favorite_food = request.POST['favorite_food']
-        user.save()
-
-    return render(request, 'main_app/index.html')
+    return render(request, 'test_app/index.html')
